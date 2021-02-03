53ef33e293fba0e87ec92644eb96e9c5ea112b1d
unknown <yestsering@gmail.com>
2021-02-03 19:59:03 +0100

    form

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const min = 1;
+const max = 10;
 class Add extends React.Component {
     constructor() {
         super();
@@ -7,12 +9,35 @@ class Add extends React.Component {
             productName : '',
             price : 1,
         }
+        this.upDatePrice = this.upDatePrice.bind(this);
+        this.upDateProductName = this.upDateProductName.bind(this);
     }
+    upDateProductName(e) {
+        this.setState ({
+            productName : this.state.productName
+        })
+    }
+    upDatePrice(event) {
+        this.setState({
+            price : event.target.value
+        })
+    }
+    
     render () {
         return (
             <div>
-                Add 
-                <input onChange=></input>
+                <input type="text" 
+                       placeholder="items"
+                       onChange={this.upDateProductName}></input>
+                <input type="button" 
+                       value="Add"></input>
+                <br></br>
+                <p>{this.state.price} €</p>
+                <input type="range" 
+                       value={this.state.price} 
+                       min={min} max={max} 
+                       onChange={this.upDatePrice}
+                       onInput={this.upDatePrice}></input>
             </div>
         )
     }
